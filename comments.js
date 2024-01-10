101dff089661a3978bc71618371884c6162c9225
Rahgir Arefin Rafi <112659971+rahgirrafi@users.noreply.github.com>
2024-01-10 10:34:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,71 @@
 //create web server
-var http = require('http');
+var express = require('express');
+var app = express();
+var port = 3000;
 var fs = require('fs');
-var url = require('url');
+var bodyParser = require('body-parser');
+var comments = require('./comments.json');
+var path = require('path');
 
-//create server
-http.createServer(function (request, response) {
-  var pathname = url.parse(request.url).pathname;
-  console.log("Request for " + pathname + " received.");
+//set up the template engine
+app.set('view engine', 'ejs');
 
-  //read file from file system
-  fs.readFile(pathname.substr(1), function (err, data) {
-    if (err) {
-      console.log(err);
-      //HTTP Status: 404 : NOT FOUND
-      //Content Type: text/plain
-      response.writeHead(404, {'Content-Type': 'text/html'});
-    } else {
-      //Page found
-      //HTTP Status: 200 : OK
-      //Content Type: text/plain
-      response.writeHead(200, {'Content-Type': 'text/html'});
+//set up the static files
+app.use(express.static('./public'));
 
-      //write content of read file to response body
-      response.write(data.toString());
-    }
+//fire body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-    //send response body
-    response.end();
-  });
-}).listen(8081);
+//listen to the port
+app.listen(port);
+console.log('You are listening to port ' + port);
 
-// Console will print the message
-console.log('Server running at http://
+//Routes
+//get the home page
+app.get('/', function(req, res){
+    res.render('index', {comments: comments});
+});
+
+//post a comment
+app.post('/', function(req, res){
+    var comment = req.body;
+    comments.push(comment);
+    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
+        if(err) throw err;
+        console.log('Saved!');
+    });
+    res.redirect('/');
+});
+
+//delete a comment
+app.delete('/delete/:id', function(req, res){
+    comments.splice(req.params.id, 1);
+    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
+        if(err) throw err;
+        console.log('Deleted!');
+    });
+    res.redirect('/');
+});
+
+//edit a comment
+app.put('/edit/:id', function(req, res){
+    var comment = req.body;
+    comments[req.params.id] = comment;
+    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
+        if(err) throw err;
+        console.log('Edited!');
+    });
+    res.redirect('/');
+});
+
+//get the edit page
+app.get('/edit/:id', function(req, res){
+    res.render('edit', {comment: comments[req.params.id], id: req.params.id});
+});
+
+//get the delete page
+app.get('/delete/:id', function(req, res){
+    res.render('delete', {comment: comments[req.params.id], id: req.params.id});
+});
 

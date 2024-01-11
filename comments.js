fda9caf536cf28cf8be64fa2c0a0c0ac4e817ffb
Rahgir Arefin Rafi <112659971+rahgirrafi@users.noreply.github.com>
2024-01-11 09:43:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,72 +1,18 @@
-//create web server
-var express = require('express');
-var app = express();
-var port = 3000;
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var comments = require('./comments.json');
-var path = require('path');
+//create a web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const commentRouter = require('./routes/comments');
+const cors = require('cors');
 
-//set up the template engine
-app.set('view engine', 'ejs');
-
-//set up the static files
-app.use(express.static('./public'));
-
-//fire body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+// parse application/json
 app.use(bodyParser.json());
+app.use('/comments', commentRouter);
 
-//listen to the port
-app.listen(port);
-console.log('You are listening to port ' + port);
-
-//Routes
-//get the home page
-app.get('/', function(req, res){
-    res.render('index', {comments: comments});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
-//post a comment
-app.post('/', function(req, res){
-    var comment = req.body;
-    comments.push(comment);
-    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
-        if(err) throw err;
-        console.log('Saved!');
-    });
-    res.redirect('/');
-});
-
-//delete a comment
-app.delete('/delete/:id', function(req, res){
-    comments.splice(req.params.id, 1);
-    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
-        if(err) throw err;
-        console.log('Deleted!');
-    });
-    res.redirect('/');
-});
-
-//edit a comment
-app.put('/edit/:id', function(req, res){
-    var comment = req.body;
-    comments[req.params.id] = comment;
-    fs.writeFile('./comments.json', JSON.stringify(comments), function(err){
-        if(err) throw err;
-        console.log('Edited!');
-    });
-    res.redirect('/');
-});
-
-//get the edit page
-app.get('/edit/:id', function(req, res){
-    res.render('edit', {comment: comments[req.params.id], id: req.params.id});
-});
-
-//get the delete page
-app.get('/delete/:id', function(req, res){
-    res.render('delete', {comment: comments[req.params.id], id: req.params.id});
-});
-//end
-
